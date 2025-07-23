45993434ddf13e756736a3d632adb712c256fe5b
JJ <xj120804@g.ucla.edu>
2025-07-22 20:54:53 -0700

    Update pipe.c

--- pipe.c
+++ pipe.c
@@ -3,6 +3,7 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <signal.h>
 
 int main(int argc, char *argv[]) {
     if (argc <= 1) exit(EINVAL);
@@ -10,69 +11,67 @@ int main(int argc, char *argv[]) {
     int n = argc - 1;
     pid_t *pids = malloc((size_t)n * sizeof(pid_t));
     if (!pids) exit(ENOMEM);
+    int *codes = malloc((size_t)n * sizeof(int));
+    if (!codes) { free(pids); exit(ENOMEM); }
+    for (int i = 0; i < n; ++i) codes[i] = -1;
 
-    int prev_read = STDIN_FILENO;
-
+    int prev = STDIN_FILENO;
     for (int i = 0; i < n; ++i) {
         int fds[2];
-        if (i != n - 1 && pipe(fds) == -1) {
-            int e = errno;
-            free(pids);
-            exit(e);
-        }
+        if (i != n - 1 && pipe(fds) == -1) { int e = errno; free(pids); free(codes); exit(e); }
 
         pid_t pid = fork();
         if (pid == -1) {
             int e = errno;
-            if (i != n - 1) {
-                close(fds[0]);
-                close(fds[1]);
-            }
-            free(pids);
-            exit(e);
+            if (i != n - 1) { close(fds[0]); close(fds[1]); }
+            free(pids); free(codes); exit(e);
         }
 
         if (pid == 0) {
-            if (prev_read != STDIN_FILENO) {
-                if (dup2(prev_read, STDIN_FILENO) == -1) _exit(errno);
-            }
-            if (i != n - 1) {
-                if (dup2(fds[1], STDOUT_FILENO) == -1) _exit(errno);
-            }
-            if (prev_read != STDIN_FILENO) close(prev_read);
-            if (i != n - 1) {
-                close(fds[0]);
-                close(fds[1]);
-            }
+            if (prev != STDIN_FILENO && dup2(prev, STDIN_FILENO) == -1) _exit(errno);
+            if (i != n - 1 && dup2(fds[1], STDOUT_FILENO) == -1) _exit(errno);
+            if (prev != STDIN_FILENO) close(prev);
+            if (i != n - 1) { close(fds[0]); close(fds[1]); }
             execlp(argv[i + 1], argv[i + 1], (char *)NULL);
             _exit(errno);
         }
 
         pids[i] = pid;
-        if (prev_read != STDIN_FILENO) close(prev_read);
+        if (prev != STDIN_FILENO) close(prev);
         if (i != n - 1) {
             close(fds[1]);
-            prev_read = fds[0];
+            prev = fds[0];
         }
     }
 
-    pid_t last_pid = pids[n - 1];
-    int waited = 0, exit_code = 0, status;
-
-    while (waited < n) {
-        pid_t w = waitpid(-1, &status, 0);
+    int remaining = n;
+    while (remaining) {
+        int status;
+        pid_t w = wait(&status);
         if (w == -1) {
             if (errno == EINTR) continue;
-            free(pids);
-            exit(errno);
+            int e = errno; free(pids); free(codes); exit(e);
         }
-        ++waited;
-        if (w == last_pid) {
-            if (WIFEXITED(status))       exit_code = WEXITSTATUS(status);
-            else if (WIFSIGNALED(status)) exit_code = 128 + WTERMSIG(status);
+        int idx = -1;
+        for (int i = 0; i < n; ++i) if (pids[i] == w) { idx = i; break; }
+        if (idx == -1) continue;
+
+        int code = 0;
+        if (WIFEXITED(status)) code = WEXITSTATUS(status);
+        else if (WIFSIGNALED(status)) {
+            int sig = WTERMSIG(status);
+            code = (sig == SIGPIPE) ? 0 : 128 + sig;
         }
+        codes[idx] = code;
+        --remaining;
+    }
+
+    int rc = 0;
+    for (int i = 0; i < n; ++i) {
+        if (codes[i] != 0) { rc = codes[i]; break; }
     }
 
     free(pids);
-    return exit_code;
+    free(codes);
+    return rc;
 }
