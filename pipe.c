6d177548cf265bde590ddf070841c5b480ac3f71
JJ <xj120804@g.ucla.edu>
2025-07-22 22:08:28 -0700

    Update pipe.c

--- pipe.c
+++ pipe.c
@@ -6,71 +6,67 @@
 #include <signal.h>
 #include <stdio.h>
 
-static int map_status(int status) {
-    if (WIFSIGNALED(status)) {
-        int sig = WTERMSIG(status);
-        return sig == SIGPIPE ? 0 : 128 + sig;
-    }
-    int code = WEXITSTATUS(status);
-    if (code == 128 + SIGPIPE) return 0;
-    return code;
+static int map_status(int s) {
+    if (WIFSIGNALED(s)) return WTERMSIG(s) == SIGPIPE ? 0 : 128 + WTERMSIG(s);
+    int c = WEXITSTATUS(s);
+    return c == 128 + SIGPIPE ? 0 : c;
 }
 
 int main(int argc, char *argv[]) {
     if (argc <= 1) exit(EINVAL);
 
-    int n = argc - 1;
-    pid_t *pids = malloc((size_t)n * sizeof(pid_t));
-    int *codes   = malloc((size_t)n * sizeof(int));
-    if (!pids || !codes) exit(ENOMEM);
-    for (int i = 0; i < n; ++i) codes[i] = -1;
+    int *start = malloc((argc - 1) * sizeof(int));
+    int *len   = malloc((argc - 1) * sizeof(int));
+    if (!start || !len) exit(ENOMEM);
+
+    int m = 0;
+    for (int i = 1; i < argc; ) {
+        start[m] = i;
+        len[m]   = 1;
+        ++i;
+        while (i < argc && argv[i][0] == '-') { ++len[m]; ++i; }
+        ++m;
+    }
+
+    pid_t *pids = malloc(m * sizeof(pid_t));
+    if (!pids) exit(ENOMEM);
 
     int prev = STDIN_FILENO;
-
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < m; ++i) {
         int fds[2];
-        if (i != n - 1 && pipe(fds) == -1) exit(errno);
+        if (i != m - 1 && pipe(fds) == -1) exit(errno);
 
         pid_t pid = fork();
         if (pid == -1) exit(errno);
 
         if (pid == 0) {
             if (prev != STDIN_FILENO && dup2(prev, STDIN_FILENO) == -1) _exit(errno);
-            if (i != n - 1 && dup2(fds[1], STDOUT_FILENO) == -1) _exit(errno);
+            if (i != m - 1 && dup2(fds[1], STDOUT_FILENO) == -1)       _exit(errno);
             if (prev != STDIN_FILENO) close(prev);
-            if (i != n - 1) { close(fds[0]); close(fds[1]); }
-            execlp(argv[i + 1], argv[i + 1], (char *)0);
-            perror(argv[i + 1]);
+            if (i != m - 1) { close(fds[0]); close(fds[1]); }
+
+            char **stage = malloc((len[i] + 1) * sizeof(char *));
+            if (!stage) _exit(ENOMEM);
+            for (int k = 0; k < len[i]; ++k) stage[k] = argv[start[i] + k];
+            stage[len[i]] = NULL;
+            execvp(stage[0], stage);
+            perror(stage[0]);
             _exit(errno == ENOENT ? 127 : 126);
         }
 
         pids[i] = pid;
         if (prev != STDIN_FILENO) close(prev);
-        if (i != n - 1) {
-            close(fds[1]);
-            prev = fds[0];
-        }
+        if (i != m - 1) { close(fds[1]); prev = fds[0]; }
     }
 
-    int remaining = n;
-    while (remaining) {
-        int status;
-        pid_t w = wait(&status);
-        if (w == -1) {
-            if (errno == EINTR) continue;
-            exit(errno);
-        }
-        int idx = -1;
-        for (int i = 0; i < n; ++i) if (pids[i] == w) { idx = i; break; }
-        if (idx == -1 || codes[idx] != -1) continue;
-        codes[idx] = map_status(status);
-        --remaining;
+    int rc = 0, left = m;
+    while (left) {
+        int st; pid_t w = wait(&st);
+        if (w == -1) { if (errno == EINTR) continue; exit(errno); }
+        int translated = map_status(st);
+        if (rc == 0 && translated) rc = translated;
+        --left;
     }
 
-    int rc = 0;
-    for (int i = 0; i < n; ++i) if (codes[i]) { rc = codes[i]; break; }
-
-    free(pids);
-    free(codes);
     return rc;
 }
