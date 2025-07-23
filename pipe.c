51b5c51db143b8577176977ed45bf82dce56fb32
JJ <xj120804@g.ucla.edu>
2025-07-22 19:48:36 -0700

    Update pipe.c

--- pipe.c
+++ pipe.c
@@ -5,7 +5,6 @@
 #include <errno.h>
 
 int main(int argc, char *argv[]) {
-    // Check if at least one program is provided
     if (argc < 2) {
         errno = EINVAL;
         exit(EINVAL);
@@ -18,7 +17,6 @@ int main(int argc, char *argv[]) {
         exit(errno);
     }
     
-    // Create pipes - we need (num_programs - 1) pipes
     int (*pipes)[2] = malloc((num_programs - 1) * sizeof(int[2]));
     if (!pipes && num_programs > 1) {
         perror("malloc");
@@ -26,7 +24,6 @@ int main(int argc, char *argv[]) {
         exit(errno);
     }
     
-    // Create all pipes before forking
     for (int i = 0; i < num_programs - 1; i++) {
         if (pipe(pipes[i]) == -1) {
             perror("pipe");
@@ -41,13 +38,11 @@ int main(int argc, char *argv[]) {
         }
     }
     
-    // Fork and execute each program
     for (int i = 0; i < num_programs; i++) {
         pids[i] = fork();
         
         if (pids[i] == -1) {
             perror("fork");
-            // Clean up pipes and wait for already created children
             for (int j = 0; j < num_programs - 1; j++) {
                 close(pipes[j][0]);
                 close(pipes[j][1]);
@@ -61,9 +56,6 @@ int main(int argc, char *argv[]) {
         }
         
         if (pids[i] == 0) {
-            // Child process
-            
-            // Set up input redirection
             if (i > 0) {
                 // Not the first process - read from previous pipe
                 if (dup2(pipes[i-1][0], STDIN_FILENO) == -1) {
@@ -71,9 +63,7 @@ int main(int argc, char *argv[]) {
                     exit(errno);
                 }
             }
-            // First process uses parent's stdin (no redirection needed)
             
-            // Set up output redirection
             if (i < num_programs - 1) {
                 // Not the last process - write to next pipe
                 if (dup2(pipes[i][1], STDOUT_FILENO) == -1) {
@@ -81,30 +71,24 @@ int main(int argc, char *argv[]) {
                     exit(errno);
                 }
             }
-            // Last process uses parent's stdout (no redirection needed)
             
-            // Close all pipe file descriptors in child
             for (int j = 0; j < num_programs - 1; j++) {
                 close(pipes[j][0]);
                 close(pipes[j][1]);
             }
             
-            // Execute the program
             execlp(argv[i + 1], argv[i + 1], (char *)NULL);
             
-            // If we reach here, execlp failed
             perror("execlp");
             exit(errno);
         }
     }
     
-    // Parent process - close all pipe file descriptors
     for (int i = 0; i < num_programs - 1; i++) {
         close(pipes[i][0]);
         close(pipes[i][1]);
     }
     
-    // Wait for all children
     int exit_status = 0;
     for (int i = 0; i < num_programs; i++) {
         int status;
@@ -112,13 +96,11 @@ int main(int argc, char *argv[]) {
             perror("waitpid");
             exit_status = errno;
         }
-        // Optionally check child exit status
         if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
             exit_status = WEXITSTATUS(status);
         }
     }
     
-    // Clean up
     free(pipes);
     free(pids);
     
