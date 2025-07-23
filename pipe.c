03ae5f504d3bfac63fdc1f9ffd594019a24558ea
JJ <xj120804@g.ucla.edu>
2025-07-22 23:22:49 -0700

    Update pipe.c

--- pipe.c
+++ pipe.c
@@ -5,78 +5,87 @@
 #include <errno.h>
 #include <signal.h>
 #include <stdio.h>
-#include <ctype.h>
-#include <string.h>
 
-static int map(int s){
-    if (WIFSIGNALED(s)) return WTERMSIG(s)==SIGPIPE?0:128+WTERMSIG(s);
-    int c=WEXITSTATUS(s); return c==128+SIGPIPE?0:c;
+static int status_map(int s) {
+    if (WIFSIGNALED(s)) {
+        int sig = WTERMSIG(s);
+        return sig == SIGPIPE ? 0 : 128 + sig;
+    }
+    int c = WEXITSTATUS(s);
+    return c == 128 + SIGPIPE ? 0 : c;
 }
 
-static int looks_cmd(const char *t){
-    if(!t||!*t) return 0;                
-    if(strlen(t)==1) return 0;           
-    if(t[0]=='-'        ) return 0;     
-    if(isdigit((unsigned char)t[0])) return 0;
-    for(const char *p=t;*p;++p)
-        if(!isalnum((unsigned char)*p) && *p!='_' && *p!='/') return 0;
-    return 1;                          
-}
-
-
-int main(int argc,char*argv[]){
-    if(argc<=1) exit(EINVAL);
-
-    int *st=malloc((argc-1)*sizeof(int));
-    int *ln=malloc((argc-1)*sizeof(int));
-    if(!st||!ln) exit(ENOMEM);
-
-    int m=0,i=1;
-    while(i<argc){
-        st[m]=i; ln[m]=1; ++i;
-        while(i<argc && !looks_cmd(argv[i])){ ++ln[m]; ++i; }
-        ++m;
+int main(int argc, char *argv[]) {
+    if (argc <= 1) {
+        errno = EINVAL;
+        perror("pipe");
+        exit(errno);
     }
 
-    pid_t *pid=malloc(m*sizeof(pid_t));
-    int *code=malloc(m*sizeof(int));
-    if(!pid||!code) exit(ENOMEM);
-    for(i=0;i<m;++i) code[i]=-1;
+    if (argc == 2) {
+        execlp(argv[1], argv[1], (char *)0);
+        perror(argv[1]);
+        exit(errno);
+    }
 
-    int prev=STDIN_FILENO;
-    for(i=0;i<m;++i){
-        int fds[2];
-        if(i!=m-1 && pipe(fds)==-1) exit(errno);
-        pid_t p=fork();
-        if(p==-1) exit(errno);
+    int prev_pipe[2] = {-1, -1};
+    pid_t *pids = malloc((argc - 1) * sizeof(pid_t));
+    int *codes  = malloc((argc - 1) * sizeof(int));
+    if (!pids || !codes) exit(ENOMEM);
+    for (int i = 0; i < argc - 1; ++i) codes[i] = -1;
 
-        if(!p){
-            if(prev!=STDIN_FILENO && dup2(prev,STDIN_FILENO)==-1) _exit(errno);
-            if(i!=m-1 && dup2(fds[1],STDOUT_FILENO)==-1) _exit(errno);
-            if(prev!=STDIN_FILENO) close(prev);
-            if(i!=m-1){ close(fds[0]); close(fds[1]); }
-
-            char **v=malloc((ln[i]+1)*sizeof(char*));
-            if(!v) _exit(ENOMEM);
-            for(int k=0;k<ln[i];++k) v[k]=argv[st[i]+k];
-            v[ln[i]]=NULL;
-            execvp(v[0],v);
-            perror(v[0]); _exit(errno);
+    for (int i = 1; i < argc; ++i) {
+        int curr_pipe[2] = {-1, -1};
+        if (i < argc - 1 && pipe(curr_pipe) == -1) {
+            perror("pipe");
+            exit(errno);
         }
 
-        pid[i]=p;
-        if(prev!=STDIN_FILENO) close(prev);
-        if(i!=m-1){ close(fds[1]); prev=fds[0]; }
+        pid_t pid = fork();
+        if (pid == -1) {
+            perror("fork");
+            exit(errno);
+        }
+
+        if (pid == 0) {
+            if (i > 1 && dup2(prev_pipe[0], STDIN_FILENO) == -1) _exit(errno);
+            if (i < argc - 1 && dup2(curr_pipe[1], STDOUT_FILENO) == -1) _exit(errno);
+
+            if (prev_pipe[0] != -1) { close(prev_pipe[0]); close(prev_pipe[1]); }
+            if (curr_pipe[0] != -1) { close(curr_pipe[0]); close(curr_pipe[1]); }
+
+            execlp(argv[i], argv[i], (char *)0);
+            perror(argv[i]);
+            _exit(errno);
+        }
+
+        pids[i - 1] = pid;
+
+        if (prev_pipe[0] != -1) { close(prev_pipe[0]); close(prev_pipe[1]); }
+        if (i < argc - 1) {
+            prev_pipe[0] = curr_pipe[0];
+            prev_pipe[1] = curr_pipe[1];
+            close(curr_pipe[1]);          /* parent keeps only read end */
+        }
     }
 
-    for(int left=m;left;){
-        int s; pid_t w=wait(&s);
-        if(w==-1){ if(errno==EINTR) continue; exit(errno); }
-        for(int k=0;k<m;++k) if(pid[k]==w){ code[k]=map(s); break; }
-        --left;
+    int remaining = argc - 1;
+    while (remaining) {
+        int st;
+        pid_t w = wait(&st);
+        if (w == -1) {
+            if (errno == EINTR) continue;
+            perror("wait");
+            exit(errno);
+        }
+        for (int k = 0; k < argc - 1; ++k)
+            if (pids[k] == w && codes[k] == -1) { codes[k] = status_map(st); break; }
+        --remaining;
     }
 
-    int rc=0;
-    for(i=0;i<m;++i) if(code[i]){ rc=code[i]; break; }
+    int rc = 0;
+    for (int k = 0; k < argc - 1; ++k)
+        if (codes[k]) { rc = codes[k]; break; }
+
     return rc;
 }
